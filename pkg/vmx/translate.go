f9be1b0415eb2456ac8381d91b17dfbe8611ca7d
John Trammell <john.trammell@gmail.com>
2022-08-25 10:01:18 -0500

    convert to new object structure

--- pkg/vmx/translate.go
+++ pkg/vmx/translate.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -14,77 +13,74 @@ import (
 // Translate .vm source code into .asm code. Create one CodeWriter object,
 // and one Parser object for each source file.
 func Translate(file string) {
-	log.Info().Msgf(`input file/dir is "%s"`, file)
-
-	basename := path.Base(file) // get filename minus path
-	outfname := strings.TrimSuffix(basename, filepath.Ext(basename)) + ".asm"
-	log.Info().Msgf(`output file is "%s"`, outfname)
-
-	// construct output filehandle
-	out, err := os.Create(outfname)
+	// get the list of source files to translate
+	srcfiles, err := GetSourceFiles(file)
 	if err != nil {
 		fmt.Println(err)
 	}
-	// close the file with defer
-	defer out.Close()
 
-	// translate all lines in all source files
-	for _, srcFileName := range getSourceFiles(file) {
-		log.Info().Msgf(`Translating source file: "%s"`, file)
-		srcFile, err := os.Open(srcFileName)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		scanner := bufio.NewScanner(srcFile)
-		for scanner.Scan() {
-			//fmt.Println(`// ` + scanner.Text())
-			cmd := NewCommand(scanner.Text())
-			out.WriteString(cmd.GetAsm())
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Println(err)
-		}
-
-		// p := NewParser(fn)
-		// for p.Scan() {
-		// 	//cw.writeComment(p.Text())
-		// 	fmt.Println(p.Scanner.Text())
-		// 	switch p.commandType() {
-		// 	case C_ARITHMETIC:
-		// 		cw.writeComment("arithmetic: " + p.Text())
-		// 		log.Info().Msgf(`looks like arithmetic`)
-		// 		cw.writeArithmetic(p.Text())
-		// 	case C_POP, C_PUSH:
-		// 		cw.writeComment("pushpop: " + p.Text())
-		// 		log.Info().Msgf(`looks like pushpop`)
-		// 		cw.writePushPop(p.commandType(), p.arg1(), p.arg2())
-		// 	default:
-		// 		cw.writeComment("unknown: " + p.Text())
-		// 		log.Info().Msgf(`unknown type`)
-		// 	}
-		// }
+	// loop over all source files, translating each
+	for _, s := range srcfiles {
+		TranslateSingleFile(s)
 	}
 }
 
-// If `src` is a directory, return all the .vm files in the directory,
-// otherwise return a single .vm filename.
-func getSourceFiles(src string) []string {
-	stat, err := os.Stat(src)
+func GetSourceFiles(input string) ([]string, error) {
+	log.Info().Msgf(`input file/dir is "%s"`, input)
+
+	// get information about the input path
+	stat, err := os.Stat(input)
 	if err != nil {
 		log.Fatal().Err(err)
 	}
 
-	// glob out the .vm files if it's a directory
+	// if `path` is a directory, glob out all the .vm files in it
 	if stat.IsDir() {
-		log.Info().Msgf(`"%s" is a directory`, src)
-		files := []string{}
-		// need to list all files matching *.vm in dir
-		return files
+		// glob out all the *.vm files
+		log.Info().Msgf(`"%s" is a dir`, input)
+		vmfiles, err := filepath.Glob(input + "/*.vm")
+		log.Info().Msgf(`*.vm glob matches: "%v"`, vmfiles)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		return vmfiles, nil
 	}
 
 	// it's not a directory, so return a slice with a single filename
-	log.Info().Msgf(`"%s" is a file`, src)
-	return []string{src}
+	log.Info().Msgf(`"%s" is a file`, input)
+	return []string{input}, nil
+}
+
+// translate VM instructions in a single file to equivalent .asm
+// Note: put this functionality in its own function to make `defer` work better
+func TranslateSingleFile(srcfile string) error {
+	outfile := strings.TrimSuffix(srcfile, filepath.Ext(srcfile)) + ".asm"
+	log.Info().Msgf(`Translating source file "%s" to "%s"`, srcfile, outfile)
+
+	// open the input file for reading
+	src, err := os.Open(srcfile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer src.Close()
+
+	// open the output file for writing
+	out, err := os.Create(outfile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer out.Close()
+
+	// read & translate the input file line by line
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		cmd := NewCommand(scanner.Text()) // FIXME: return pointer?
+		if asm := cmd.GetAsm(); len(asm) > 0 {
+			out.WriteString(cmd.GetAsm() + "\n")
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	return err
 }
