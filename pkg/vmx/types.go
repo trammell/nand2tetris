f1d4aa5abf2c5838090e63873fc1ca9fca26f71b
John Trammell <john.trammell@gmail.com>
2022-08-25 10:03:33 -0500

    clean up types.go to reflect new OO structure

--- pkg/vmx/types.go
+++ pkg/vmx/types.go
@@ -1,38 +1,6 @@
 package vmx
 
-import (
-	"bufio"
-	"io"
-)
-
-// These are all our command types
-const (
-	C_ARITHMETIC = iota
-	C_PUSH
-	C_POP
-	C_LABEL
-	C_GOTO
-	C_IF
-	C_FUNCTION
-	C_RETURN
-	C_CALL
-	C_UNDEFINED
-)
-
 type Command struct {
 	vmCommand string
-	fields []string
-}
-
-type Parser struct {
-	Source  string
-	Scanner *bufio.Scanner
-}
-
-type CodeWriter struct {
-	Writer io.Writer
-}
-
-type Translatable interface {
-	GetAsm() string
+	fields    []string
 }
